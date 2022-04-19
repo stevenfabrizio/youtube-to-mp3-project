805c59a816772bea58b7066f1dcd67760e8e2d68
shfabrizio <shfabrizio@gmail.com>
2022-04-19 07:41:02 -0400

    putting each url into their own function

--- server/routes/download.js
+++ server/routes/download.js
@@ -9,44 +9,34 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 
 
 router.get('/', (req, res) => { 
-    let url1 = req.query.urlONE;
-    let finalUrl1 = url1.split('=')
-    let finalUrl11 = finalUrl1[1].split('&')
-    console.log(url1 + ' becomes ' + finalUrl11[0])
-
-    let url2 = req.query.urlTWO;
-    let finalUrl2 = url2.split('=')
-    let finalUrl22 = finalUrl2[1].split('&')
-    console.log(url2 + ' becomes ' + finalUrl22[0])
-
-    let name1 = req.query.filename1
-    if (req.query.filename1===''){
-        name1 = 'MP3_Number_1'
-    }
-    name1 = name1.split(" ").join("_")
-
-    let name2 = req.query.filename2
-    if( req.query.filename2===''){
-        name2 = 'MP3_Number_2'
-    }
-    name2 = name2.split(" ").join("_")
-
-
-    let stream = ytdl(finalUrl11[0], {
-        quality: 'highestaudio',
-    });
-    let stream2 = ytdl(finalUrl22[0], {
-        quality: 'highestaudio',
-    });
-
-    ytdl.getInfo(req.query.urlONE).then(info => {
-        let tempString = info.videoDetails.title
-        console.log(tempString)
-    })
 
     const ConvertUrl1 = () => {
         return new Promise((resolve, reject) => {
+
+            let url1 = req.query.urlONE;
+            if (url1 === ''){
+                return
+            }
+            let finalUrl1 = url1.split('=')
+            let finalUrl11 = finalUrl1[1].split('&')
+            console.log(url1 + ' becomes ' + finalUrl11[0])
+
+            let name1 = req.query.filename1
+            if (req.query.filename1===''){
+                name1 = 'MP3_Number_1'
+            }
+            name1 = name1.split(" ").join("_")
+
+            let stream = ytdl(finalUrl11[0], {
+                quality: 'highestaudio',
+            });
+
             let start1 = Date.now();
+
+            ytdl.getInfo(req.query.urlONE).then(info => {
+                let tempString = info.videoDetails.title
+                console.log(tempString)
+            })
 
             console.log('Starting no 1...')
 
@@ -73,6 +63,24 @@ router.get('/', (req, res) => {
         const result = await ConvertUrl1()  
         result
 
+        let url2 = req.query.urlTWO;
+        if (url2 === ''){
+            return
+        }
+        let finalUrl2 = url2.split('=')
+        let finalUrl22 = finalUrl2[1].split('&')
+        console.log(url2 + ' becomes ' + finalUrl22[0])
+    
+        let name2 = req.query.filename2
+        if( req.query.filename2===''){
+            name2 = 'MP3_Number_2'
+        }
+        name2 = name2.split(" ").join("_")
+    
+        let stream2 = ytdl(finalUrl22[0], {
+            quality: 'highestaudio',
+        });
+    
         let start2 = Date.now(); 
 
         console.log('Starting no 2...')
@@ -92,8 +100,11 @@ router.get('/', (req, res) => {
             })
             .on('end', () => {
                 console.log(`\nNo 2 Done! - ${(Date.now() - start2) / 1000}s`);
+
+                return
             });
     }
+
     ConvertUrl2()
 })
 
