823d22f28b1c313f41006bb4bacc5ee7b395e164
guitarisprettierthanpiano <shfabrizio@gmail.com>
2022-04-25 05:57:27 -0400

    cleaning up css and loadstorage.js

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # youtube-to-mp3-project
 
-I made this app because I am sick of converters that only do one file at a time. Here you can do five at a time and search five more while those are downloading.
+I made this app because I am sick of converters that only do one file at a time. Here you can do five at a time and search five more while those are converting.
 
 How to run this app:
 
@@ -10,11 +10,12 @@ How to run this app:
 4. npm run nodemon
 5. Go to http://localhost:8000/
 6. Copy and paste youtube links into the app
-7. Check server/converted_files folder for MP3s
+7. Add a name to your file if you like. Defaults to YT title if not entered.
+8. Check server/converted_files folder for MP3s
 
 ![HTML Page](https://cdn.discordapp.com/attachments/840740146176851979/967203450318377080/unknown.png)
 
 ![Terminal](https://cdn.discordapp.com/attachments/840740146176851979/967229392868753448/unknown.png)
 
-background image is from figma but i edited it a bit:
+Background image is from this figma pack but I edited it a bit:
 https://www.figma.com/community/file/1042176651593835021
--- server/public/loadStorage.js
+++ server/public/loadStorage.js
@@ -1,28 +1,41 @@
 //array of all inputs using values
-const storageArray = ['filename1', 'filename2', 'filename3', 'filename4', 'filename5', 'url1', 'url2', 'url3', 'url4', 'url5']
+const storageArray = [
+  'filename1',
+  'filename2',
+  'filename3',
+  'filename4',
+  'filename5',
+  'url1',
+  'url2',
+  'url3',
+  'url4',
+  'url5',
+];
 
 //if local storage exists, set the value of the htmlelement to it.
-for (let i = 0; i < storageArray.length; i++){
-    if(typeof localStorage.getItem(`${storageArray[i]}`) === 'string'){
-        document.getElementById(`${storageArray[i]}`).value = localStorage.getItem(`${storageArray[i]}`)
-    }
+for (let i = 0; i < storageArray.length; i++) {
+  if (typeof localStorage.getItem(`${storageArray[i]}`) === 'string') {
+    document.getElementById(`${storageArray[i]}`).value = localStorage.getItem(
+      `${storageArray[i]}`
+    );
+  }
 }
 
-//clear all button
-const clearbtn = document.getElementById('clear-btn')
-clearbtn.addEventListener('click', function(){
-    localStorage.clear()
+//clear all button. clears localstorage and input values.
+const clearbtn = document.getElementById('clear-btn');
+clearbtn.addEventListener('click', function () {
+  localStorage.clear();
 
-    for (let i = 0; i < storageArray.length; i++){
-        document.getElementById(`${storageArray[i]}`).value = ''
-    }
-})
+  for (let i = 0; i < storageArray.length; i++) {
+    document.getElementById(`${storageArray[i]}`).value = ''; 
+  }
+});
 
 //clicked convert button. if url1 value nonempty, add loading animation to button.
-const convertbtn = document.getElementById('convert-btn')
-convertbtn.addEventListener('click', function(){
-    if(document.getElementById('url1').value !== ''){
-        convertbtn.classList.add('clicked')
-        convertbtn.value = '...'
-    }
-})
+const convertbtn = document.getElementById('convert-btn');
+convertbtn.addEventListener('click', function () {
+  if (document.getElementById('url1').value !== '') {
+    convertbtn.classList.add('clicked');
+    convertbtn.value = '...';
+  }
+});
